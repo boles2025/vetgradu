05404e3d1f1b6d4d7f97db968ff463d55742eaac
boles2025 <bolesgaras200000@gmail.com>
2025-08-11 23:37:42 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBtLojF4wpmpDua3UZa_GzlaXhTxM2ediI",
+  authDomain: "gradu-ae76f.firebaseapp.com",
+  projectId: "gradu-ae76f",
+  storageBucket: "gradu-ae76f.firebasestorage.app",
+  messagingSenderId: "564230686481",
+  appId: "1:564230686481:web:4f219e913ee1185ba32eb0"
+};
